37c394d5e3b11e233b1961876e08ba180a217dc5
handya <handya@gmail.com>
2020-09-11 09:00:40 +1200

    feat: add try flatmap

---
+++ Sources/VaporToolKit/EventLoopFuture+tryFlatMap.swift
@@ -0,0 +1,24 @@
+//
+//  EventLoopFuture+tryFlatMap.swift
+//  
+//
+//  Created by Andrew Farquharson on 11/09/20.
+//
+
+import NIO
+
+/// Author: vzsg (Discord) - https://twitter.com/vzsg_dev
+/// Source: https://discordapp.com/channels/431917998102675485/684159753189982218/684537099378098272
+extension EventLoopFuture {
+    func tryFlatMap<NewValue>(file: StaticString = #file,
+                              line: UInt = #line,
+                              _ callback: @escaping (Value) throws -> EventLoopFuture<NewValue>) -> EventLoopFuture<NewValue> {
+        return flatMap(file: file, line: line) { result in
+            do {
+                return try callback(result)
+            } catch {
+                return self.eventLoop.makeFailedFuture(error, file: file, line: line)
+            }
+        }
+    }
+}
