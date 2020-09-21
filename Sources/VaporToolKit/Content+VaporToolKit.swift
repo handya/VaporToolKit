d9f77ee18a94513423d916c568302559f9ae3eea
Andrew Farquharson <handya@gmail.com>
2020-09-21 14:37:32 +1200

    feat: raw query also decode

--- Sources/VaporToolKit/Content+VaporToolKit.swift
+++ Sources/VaporToolKit/Content+VaporToolKit.swift
@@ -16,11 +16,25 @@ public extension Content {
         return sqldb.raw(SQLQueryString(query)).all(decoding: Self.self)
     }
 
+    static func rawQueryAll<A>(_ query: String, alsoDecode: A.Type, on db: Database) -> EventLoopFuture<[(Self, A)]> where A: Decodable {
+        guard let sqldb = db as? SQLDatabase else {
+            return db.eventLoop.makeFailedFuture(Abort(.internalServerError))
+        }
+        return sqldb.raw(SQLQueryString(query)).all(decoding: Self.self, A.self)
+    }
+
     static func rawQueryFirst(_ query: String, on db: Database) -> EventLoopFuture<Self?> {
         guard let sqldb = db as? SQLDatabase else {
             return db.eventLoop.makeFailedFuture(Abort(.internalServerError))
         }
         return sqldb.raw(SQLQueryString(query)).first(decoding: Self.self)
+    }
+
+    static func rawQueryFirst<A>(_ query: String, alsoDecode: A.Type, on db: Database) -> EventLoopFuture<(Self, A)?> where A: Decodable {
+        guard let sqldb = db as? SQLDatabase else {
+            return db.eventLoop.makeFailedFuture(Abort(.internalServerError))
+        }
+        return sqldb.raw(SQLQueryString(query)).first(decoding:  Self.self, A.self)
     }
 
     static func rawQueryExists(_ query: String, on db: Database) -> EventLoopFuture<Bool> {
